f4e94c022096783f7a218eaa17ca81a9fd6cd06d
KyleRobins <darkspidersteewy@gmail.com>
2022-07-01 09:05:31 +0300

    4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,8 @@
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
-
 int main(void)
 {
-    // your code goes here
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
